e8239f7f665e953a9f9b020d337ad3d893956b1a
matheuszarpellon <mvzarpellon@gmail.com>
2021-06-29 04:46:50 -0300

    aula 2

---
+++ main.js
@@ -0,0 +1,33 @@
+/* abrir e fechar menu quando clica no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', () => {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', () => {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY >= navHeight) {
+    //maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //menor que a altura do header
+    header.classList.remove('scroll')
+    }
+  }
+)
